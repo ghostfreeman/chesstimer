90e12c30b3411cb57476c5a8d7089ea51d0da479
Cameron Kilgore <ghostfreeman@gmail.com>
2015-09-18 21:04:37 -0400

    Breakpoint

--- js/timer.js
+++ js/timer.js
@@ -7,12 +7,10 @@
  * Constructor for the Timer Class
  * @param {[type]} name         [description]
  * @param {[type]} timerLength  Maximimum countdown length of the timer in seconds (default 60 seconds)
- * @param {[type]} timerCounter [description]
  */
-function Timer(name, timerLength, timerCounter) {
+function Timer(name, timerLength) {
   name = name;
   length = typeof length !== 'undefined' ? counter : 60;
-  counter = typeof timerCounter !== 'undefined' ? counter : 1;
   remaining = timerLength;
 }
 
